a5a3c71ac4e5a8b667b243fc3482478c2f06eae9
Jaeman Cho <57745706+JaemanCho@users.noreply.github.com>
2019-11-26 18:10:40 +0900

    Create MVC for projects table using

---
+++ resources/views/projects/index.blade.php
@@ -0,0 +1,13 @@
+@extends('projects.layout')
+
+@section('title')
+Projects
+@endsection
+
+@section('content')
+    <h1>Project List</h1>
+    @foreach ($projects as $project)
+        Title: {{ $project->title }} <br>
+        Description: {{ $project->description }} <br>      
+    @endforeach
+@endsection
