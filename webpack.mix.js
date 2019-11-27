8e507d44752121aaea207a76eb16031621c5f690
Jaeman Cho <57745706+JaemanCho@users.noreply.github.com>
2019-11-27 09:28:14 +0900

    Install Tailwind via npm && npm run dev

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,7 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+   .sass('resources/assets/sass/app.scss', 'public/css')
+   .postCss('resources/css/tailwind.css', 'public/css', [
+      require('tailwindcss'),
+    ]);
